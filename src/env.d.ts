7a1fda4836789487864078f2ac9c8f9541fde3c0
HEEPOKE <Damon1fx@gmail.com>
2023-04-24 17:21:46 +0700

    :money_with_wings: set env

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
-/// <reference types="astro/client" />
+/// <reference types="astro/client-image" />
+/// <reference types="astro/client-image" />
+
+interface ImportMetaEnv {
+    readonly ENDPOINT_URL: string;
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
